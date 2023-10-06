aaed614ea7b6cb1113833bdb844d0a309ca63422
crisyg97 <cristiangaray101@gmail.com>
2023-10-06 11:41:52 -0300

    añadido archivos base

---
+++ arbolBinario.asm
@@ -0,0 +1,17 @@
+
+
+section .text
+
+
+
+push ebp 	 ;guardo base point
+mov ebp, esp ;el nuevo base point es stack point
+
+//manejo de la pila
+mov ebx, [ebp + 8]  ;Obtener el puntero al nodo izquierdo
+mov ecx, [ebp + 12] ;Obtener el puntero al nodo derecho
+mov edx, [ebp + 16] ;Obtener el valor del nodo
+
+mov [eax], ebx      ;Asignar el puntero al nodo izquierdo
+mov [eax + 4], ecx  ;Asignar el puntero al nodo derecho
+mov [eax + 8], edx  ;Asignar el valor del nodo
