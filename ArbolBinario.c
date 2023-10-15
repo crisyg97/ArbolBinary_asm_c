04448c8968dd8c74363016f6ba49bba1fc87ea47
crisyg97 <cristiangaray101@gmail.com>
2023-10-15 10:45:17 -0300

    fix code

--- ArbolBinario.c
+++ ArbolBinario.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //extern free
 //extern malloc
@@ -8,21 +9,19 @@ extern struct Nodo* crearArbolB(struct Nodo* izq, struct Nodo* der, int val);
 //call malloc
 //call free
 
-ab* crearArbolB(int valor){ //return pos memoria de un arbol
+/*ab* crearArbolB(int valor){ //return pos memoria de un arbol
 	
 }
 
 ab* crearArbol(int valor, ab* izq, ab* der){//return pos memoria de un arb
 	
-}
-
-void printArbolBinario(ab)
+}*/
 
 struct nodo{
    int dato;
    struct nodo *izq;
    struct nodo *der;
-}
+};
 
 struct nodo *newNodo(int dato){
    size_t tamNodo = sizeof(struct nodo);
@@ -35,9 +34,9 @@ struct nodo *newNodo(int dato){
 int main(){
 	struct Nodo* nodoIzq = (struct Nodo*)malloc(sizeof(struct Nodo));
 	struct Nodo* nodoDer = (struct Nodo*)malloc(sizeof(struct Nodo));
-	struct Nodo* rizq = crearArbolB(nodoIzq, nodoDer, 5);
-	struct Nodo* rder = crearArbolB(nodoIzq, nodoDer, 7);
-	struct Nodo* raiz = crearArbolB(rizq, rder, 10);
+	struct Nodo* rizq = crearArbolB(nodoIzq, nodoDer, 5); //llamada a la funcion en asm
+	struct Nodo* rder = crearArbolB(nodoIzq, nodoDer, 7);//llamada a la funcion en asm
+	struct Nodo* raiz = crearArbolB(rizq, rder, 10); //llamada a la funcion en asm
 	
 	printf("Valor del nodo: %d\n", raiz->valor);
 	printf("Valor del nodo izquierdo: %d\n", raiz->izquierdo->valor); //accediendo al valor del nodo izquierdo de la raiz del arbol binario
--- arbolBinario.asm
+++ arbolBinario.asm
@@ -1,9 +1,20 @@
+global crearArbolB
+extern _malloc
+extern _free
 
+section .data
+    izq dd 0
+    der dd 0
+    dato dd 0
+    SizeofNodo equ 12
 
 section .text
 
+agregarNodo:
+    
 
 main:
+    ;enter
     push ebp 	 ;guardo base point
     mov ebp, esp ;el nuevo base point es stack point
 
@@ -15,3 +26,8 @@ main:
     mov [eax], ebx      ;Asignar el puntero al nodo izquierdo
     mov [eax + 4], ecx  ;Asignar el puntero al nodo derecho
     mov [eax + 8], edx  ;Asignar el valor del nodo
+
+    ;leave
+    mov esp, ebp ;reinicio el ebp a la posicion de partida
+    pop ebp ;restauro el viejo ebp
+    ret
